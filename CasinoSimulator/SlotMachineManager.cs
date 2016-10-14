b36d60aae29cedbcc2d78e7a7e18f53fab124585
Martin Kierkegaard <mark@easj.dk>
2016-10-14 15:47:00 +0200

    Add project files.

---
+++ CasinoSimulator/SlotMachineManager.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CasinoSimulator
+{
+    // This class manages a simulation of the slot machine.
+    // Its main responsibility is to communicate with the player,
+    // and keep the game running as long as the player wants
+    class SlotMachineManager
+    {
+        // Only one instance variable; a slot machine simulator
+        // private SlotMachineSimulator theSimulator;
+
+        public SlotMachineManager()
+        {
+            // theSimulator = new SlotMachineSimulator();
+        }
+
+
+        // This method runs the slot machine simulation.
+        // As long as the player wants to play (and has money left),
+        // a new spin on the slot machine is done
+        public void RunSimulation()
+        {
+            SlotMachineSimulator theSimulator = new SlotMachineSimulator();
+
+            // Reset the simulator, so we start from the initial state
+            // theSimulator.Reset();
+
+            Console.WriteLine("Slot Machine Simulator Starting...");
+            Console.WriteLine();
+
+            // Print the winning table, for the players convenience
+            theSimulator.PrintWinningTable();
+
+            // Add credits to play for (users choice)
+            int initialCredits = AskPlayerToEnterANumber("Enter number of credits to play for ");
+            theSimulator.AddCredits(initialCredits);
+
+            // The main loop: while the player still wants to play - and
+            // has money left - a new spin is performed
+            bool playAgain = true;
+            while (playAgain && (theSimulator.GetCredits() > 0))
+            {
+                // Do another spin on the machine
+                theSimulator.Spin();
+
+                if (theSimulator.GetCredits() > 0)
+                {
+                    if (theSimulator.GetCredits() < 10)
+                    {
+                        Console.WriteLine("WARNING! You have less than 10 credits left!yy");
+                    }
+
+                    // The player has money left, so ask the player
+                    // if (s)he wants to play again
+                    playAgain = AskPlayerToPlayOrQuit();
+                }
+                else
+                {
+                    // Player has no money left, so the game must stop
+                    Console.WriteLine("Sorry, you have no money left...");
+                    Console.WriteLine();
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine("Slot Machine Simulator Ending...");
+            Console.WriteLine();
+        }
+
+        // Run a long simulation, possibly in silent mode
+        public void RunLongSimulation(int noOfCredits, int noOfSpins, bool silentMode)
+        {
+            SlotMachineSimulator theSimulator = new SlotMachineSimulator();
+            theSimulator.AddCredits(noOfCredits);
+
+            theSimulator.SetSilentMode(silentMode);
+            for (int spinNo = 0; spinNo < noOfSpins; spinNo++)
+            {
+                theSimulator.Spin();
+            }
+            theSimulator.SetSilentMode(false);
+
+            Console.WriteLine();
+            Console.WriteLine("Simulation done, Statistics:");
+            Console.WriteLine("Did {0} spins", noOfSpins);
+            Console.WriteLine("Started with {0} credits", noOfCredits);
+            Console.WriteLine("Ended with {0} credits", theSimulator.GetCredits());
+            Console.WriteLine();
+            theSimulator.PrintStatistics();
+        }
+
+        // This method asks the player if (s)he wants to play again.
+        // If the method returns true, the player wants to play again.
+        // Otherwise, the player wants to quit.
+        // You do not need to understand the details of this method...
+        private bool AskPlayerToPlayOrQuit()
+        {
+            // Ask the player if (s)he wants to play again.
+            // The only acceptable answer is to press either "y" or "n"
+            Console.Write("Play again? (press y for Yes, n for No) : ");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine();
+
+            // keyStr now holds the players response
+            String keyStr = key.KeyChar.ToString();
+
+            if (keyStr == "y")
+            {
+                // Player pressed "y", so we play again
+                return true;
+            }
+            else if (keyStr == "n")
+            {
+                // Player pressed "n", so we do NOT play again
+                return false;
+            }
+            else
+            {
+                // Player entered something else than "y" or "n",
+                // so ask the user again...
+                Console.WriteLine("Please enter either y or n");
+                return AskPlayerToPlayOrQuit();
+            }
+        }
+
+        // This method can be used to get a number from the player.
+        // The text given as parameter is displayed to the player.
+        // The method will keep asking until the player enters a number.
+        // You do not need to understand the details of this method...
+        private int AskPlayerToEnterANumber(string textToShowUser)
+        {
+            Console.WriteLine();
+            Console.Write(textToShowUser + ": ");
+            string answer = Console.ReadLine();
+
+            int number = 0;
+            try
+            {
+                number = Int32.Parse(answer);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid number...");
+                AskPlayerToEnterANumber(textToShowUser);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ CasinoSimulator/SlotMachineSimulator.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CasinoSimulator
+{
+    // This class is supposed to simulate a classic slot machine.
+    // The slot machine has three "dials"; each dial can show:
+    // 7 : "Seven"
+    // # : "Sharp"
+    // @ : "At"
+    // (and later on a '*' as well)
+    //
+    // The player can enter "credits" into the machine.
+    // Each spin on the machine costs one credit.
+    // Certain combinations of the three dials gives the player
+    // a winning.
+    //
+    class SlotMachineSimulator
+    {
+        // INSTANCE VARIABLES
+
+        // The numbers of credits left in the machine
+        private int credits;
+
+        // The three dials on the slot machine
+        private string dial1;
+        private string dial2;
+        private string dial3;
+
+        // This instance variable is used for generating random numbers
+        private Random generator;
+
+        // Defines if the simulation should run in silent mode (no printouts)
+        private bool silentModeOn;
+
+        private int gotStar3;
+        private int gotSeven3;
+        private int gotSharp3;
+        private int gotAt3;
+        private int gotSeven2;
+        private int gotSharp2;
+        private int gotNothing;
+
+        // CONSTANTS
+
+        // The probabilities that a dial shows a certain symbol:
+        //  2 % for showing "*"
+        //  8 % for showing "7"
+        // 30 % for showing "#"
+        // 60 % for showing "@"
+        //
+        private const int probStar = 2;
+        private const int probSeven = 8;
+        private const int probSharp = 30;
+        private const int probAt = 60;
+
+        // The winnings paid to the player for certain dial combinations
+        // * * * pays 10000
+        // 7 7 7 pays 150
+        // # # # pays 10
+        // @ @ @ pays 1
+        // any two 7 pays 5
+        // any two # pays 1
+        //
+        private const int winningStar3 = 10000;
+        private const int winningSeven3 = 150;
+        private const int winningSharp3 = 10;
+        private const int winningAt3 = 1;
+        private const int winningSeven2 = 5;
+        private const int winningSharp2 = 1;
+    
+
+
+        // CONSTRUCTOR
+        //
+        public SlotMachineSimulator()
+        {
+            silentModeOn = false;
+            generator = new Random();
+            Reset();
+        }
+ 
+
+        // PUBLIC METHODS
+
+        // Sets the simulator back to its starting state
+        public void Reset()
+        {
+            credits = 0;
+
+            gotStar3 = 0;
+            gotSeven3 = 0;
+            gotSharp3 = 0;
+            gotAt3 = 0;
+            gotSeven2 = 0;
+            gotSharp2 = 0;
+            gotNothing = 0;
+        }
+
+        public void SetSilentMode(bool silentModeOn)
+        {
+            this.silentModeOn = silentModeOn;
+        }
+
+        public bool GetSilentMode()
+        {
+            return silentModeOn;
+        }
+
+        // Returns the number of credits left in the machine
+        public int GetCredits()
+        {
+            return credits;
+        }
+    
+        // Adds a number of credits to the machine
+        public void AddCredits(int moreCredits)
+        {
+            credits = credits + moreCredits;
+        }
+
+        // Perform one spin on the machine
+        public void Spin()
+        {
+            PrintMessageBeforeSpin();
+
+            // One spin costs one credit
+            credits--;
+        
+            // Spin the dials
+            SpinAllDials();
+        
+            // Find the winnings, and update the credits left
+            int winnings = CalculateWinnings(dial1,dial2,dial3);
+            credits = credits + winnings;
+
+            PrintOutcomeOfSpin(winnings);
+        }
+
+        private void PrintMessageBeforeSpin()
+        {
+            // Inform the player how many credits are left before spinning
+            WriteLineConditional();
+            WriteLineConditional("Spinning, credits left : " + credits);
+        }
+
+        private void SpinAllDials()
+        {
+            // Spin the dials
+            dial1 = SpinDial();
+            dial2 = SpinDial();
+            dial3 = SpinDial();
+        }
+
+        private void PrintOutcomeOfSpin(int winnings)
+        {
+            // Report the outcome of the spin to the player
+            WriteLineConditional("You got : " + dial1 + " " + dial2 + " " + dial3);
+
+            if (winnings == 0)
+            {
+                WriteLineConditional("Sorry, you did not win anything");
+            }
+            else if (winnings == 1)
+            {
+                WriteLineConditional("You won 1 credit");
+            }
+            else
+            {
+                WriteLineConditional("You won " + winnings + " credits, congratulations!");
+            }
+
+            WriteLineConditional("Credits left : " + credits);
+        }
+
+        // Print the complete winning table
+        public void PrintWinningTable()
+        {
+            WriteLineConditional("----------- Winning table for slot machine --------------");
+            WriteLineConditional(" * * * pays      " + winningStar3);
+            WriteLineConditional(" 7 7 7 pays      " + winningSeven3);
+            WriteLineConditional(" # # # pays      " + winningSharp3);
+            WriteLineConditional(" @ @ @ pays      " + winningAt3);
+            WriteLineConditional(" any two 7 pays  " + winningSeven2);
+            WriteLineConditional(" any two # pays  " + winningSharp2);
+            WriteLineConditional("---------------------------------------------------------");
+            WriteLineConditional();
+        }
+
+        public void PrintStatistics()
+        {
+            WriteLineConditional("----------- Statistics for slot machine -----------------");
+            WriteLineConditional(" * * * seen      " + gotStar3 + " times");
+            WriteLineConditional(" 7 7 7 seen      " + gotSeven3 + " times");
+            WriteLineConditional(" # # # seen      " + gotSharp3 + " times");
+            WriteLineConditional(" @ @ @ seen      " + gotAt3 + " times");
+            WriteLineConditional(" any two 7 seen  " + gotSeven2 + " times");
+            WriteLineConditional(" any two # seen  " + gotSharp2 + " times");
+            WriteLineConditional(" No win    seen  " + gotNothing + " times");
+            WriteLineConditional("---------------------------------------------------------");
+        }
+
+
+        // PRIVATE METHODS
+
+        // Generate a "percentage", i.e. a number between 0 and 100 (100 not included)
+        private int GeneratePercentage()
+        {
+            int result = generator.Next(100);
+            return result;
+        }
+
+        // Spin a dial, using the defined percentages
+        private string SpinDial()
+        {
+            // Generate a random percentage
+            int percentage = GeneratePercentage();
+
+            // Given the random percentage, find out what the dial should show
+            //
+            if (percentage < probStar)
+            {
+                return "*";
+            }
+            else if (percentage < (probStar + probSeven))
+            {
+                return "7";
+            }
+            else if (percentage < (probStar + probSeven + probSharp))
+            {
+                return "#";
+            }
+            else
+            {
+                return "@";
+            }
+        }
+
+        // Calculate the winnings corresponding to the given dial combination
+        private int CalculateWinnings(string dial1, string dial2, string dial3)
+        {
+            if (CountSymbols("*", dial1, dial2, dial3) == 3)
+            {
+                gotStar3++;
+                return winningStar3;
+            }
+
+            if (CountSymbols("7", dial1, dial2, dial3) == 3)
+            {
+                gotSeven3++;
+                return winningSeven3;
+            }
+            else if (CountSymbols("#", dial1, dial2, dial3) == 3)
+            {
+                gotSharp3++;
+                return winningSharp3;
+            }
+            else if (CountSymbols("@", dial1, dial2, dial3) == 3)
+            {
+                gotAt3++;
+                return winningAt3;
+            }
+            else if (CountSymbols("7", dial1, dial2, dial3) == 2)
+            {
+                gotSeven2++;
+                return winningSeven2;
+            }
+            else if (CountSymbols("#", dial1, dial2, dial3) == 2)
+            {
+                gotSharp2++;
+                return winningSharp2;
+            }
+            else
+            {
+                gotNothing++;
+                return 0;
+            }
+        }
+
+        // A helper method for counting how many of the three strings that
+        // are equal to the "target" string
+        private int CountSymbols(string target, string c1, string c2, string c3)
+        {
+            int count = 0;
+
+            if (target == c1) count++;
+            if (target == c2) count++;
+            if (target == c3) count++;
+
+            return count;
+        }
+
+        private void WriteLineConditional(string message)
+        {
+            if (!silentModeOn)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        private void WriteLineConditional()
+        {
+            WriteLineConditional("");
+        }
+    }
+}
